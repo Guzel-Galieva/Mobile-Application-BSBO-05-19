6ec7987179a5795d88da1920f54287352b04c01d
Guzel-Galieva <guzel.vv.com@gmail.com>
2021-05-20 14:08:35 +0300

    Initial commit

---
+++ myapplication/src/main/java/com/mirea/galieva/myapplication/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.mirea.galieva.myapplication.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
